c464c27e50999cc6e8e04aa6c545089d37d59ef0
Joel Diberius <joeldiberius@gmail.com>
2023-03-30 01:17:58 +0200

    Done with game()

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 let computerScore = 0;
 let playerScore = 0;
-// let roundWinner = "";
+let roundWinner = "";
 
-
+function game() {
+  console.log("Welcome!");
+  while (playerScore < 5 && computerScore < 5) {
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    if (roundWinner == "player") {
+      playerScore++;
+    }
+    if (roundWinner == "computer") {
+      computerScore++;
+    }
+    console.log(`PlayerScore: ${playerScore} \t ComputerScore: ${computerScore}`);
+  }
+  console.log("Game Over!");
+  if (playerScore > computerScore) {
+    console.log("The winner is... \n THE PLAYER!!!");
+  }
+  if (playerScore < computerScore) {
+    console.log("The winner is... \n THE COMPUTER!!!");
+  }
+}
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    // roundWinner = "Tie!";
-    console.log('Tie!');
+    roundWinner = "Tie!";
+    return "Tie!";
   }
 
   if (
@@ -15,9 +36,8 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "scissors" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "rock")
   ) {
-    playerScore++;
-    // roundWinner = "player";
-    console.log('Player Wins!');
+    roundWinner = "player";
+    return "Player Wins!";
   }
 
   if (
@@ -25,9 +45,8 @@ function playRound(playerSelection, computerSelection) {
     (computerSelection === "scissors" && playerSelection === "paper") ||
     (computerSelection === "paper" && playerSelection === "rock")
   ) {
-    computerScore++;
-    // roundWinner = "computer";
-    console.log('Computer Wins!');
+    roundWinner = "computer";
+    return "Computer Wins!";
   }
 }
 
@@ -43,11 +62,10 @@ function getComputerChoice() {
   }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+function getPlayerChoice() {
+  let choice = prompt("rock, paper or scissors");
+  let choiceInLower = choice.toLowerCase();
+  return choiceInLower;
+}
 
-console.log(playRound(playerSelection, computerSelection));
-
-console.log(`Player chose ${playerSelection}!`);
-console.log(`Computer chose ${computerSelection}!`);
-console.log(`Player: ${playerScore} \t Computer: ${computerScore}`);
+game();
